f329acebb4b87552da45f8a8f33f7d447fed00b4
Tommaso Bona <piergeolo@gmail.com>
2023-08-13 15:47:52 +0200

    Update poison.py

--- poison.py
+++ poison.py
@@ -121,7 +121,7 @@ class ReportThread(QThread):
                 **self.other_headers,
             }
 
-            response = requests.get(self.url, headers=headers)
+            response = requests.post(self.url, headers=headers)
 
             if response.status_code == 200:
                 total_reports += 1
